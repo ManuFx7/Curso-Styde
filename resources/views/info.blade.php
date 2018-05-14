cb4036aa18930a44d95499d66e6ca45ef71e4472
Manu Paez <manu9lin@gmail.com>
2018-05-14 21:45:11 +0200

    Fin Layouts

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Info de Usuario Administrador</title>
-</head>
-<body>
+@extends('layout')
+ @section('content')
 	<h1> Info </h1>
 	<ul>
 	@forelse($admin as $adm)
@@ -12,5 +8,4 @@
 	<hr><h2>No tienes acceso aqui </h2>
 	@endforelse
 	</ul>
-</body>
-</html>
+ @endsection
